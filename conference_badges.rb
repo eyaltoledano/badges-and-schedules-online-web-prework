519a5fe2bffa7ef30ee19328d90c8b80377026f1
Eyal Toledano <eyaltoledano@me.com>
2019-01-10 23:31:13 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,17 +4,13 @@ end
 
 def batch_badge_creator(name_array)
   new_array = []
-  name_array.each.with_index { |name|
-    new_array << "Hello, my name is " + name + "."
-  }
+  name_array.each.with_index { |name| new_array << "Hello, my name is " + name + "." }
   new_array
 end
 
 def assign_rooms(name_array)
   new_array = []
-  name_array.each.with_index { |name, room|
-    new_array << "Hello, " + name + "! You'll be assigned to room " + (room + 1).to_s + "!"
-  }
+  name_array.each.with_index { |name, room| new_array << "Hello, " + name + "! You'll be assigned to room " + (room + 1).to_s + "!" }
   new_array
 end
 
